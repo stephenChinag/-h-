39daeeb7debd3bd2ade7a7b0abaad80f19a12387
stephenChinag <stephenchinag@gmail.com>
2024-08-14 09:46:38 +0100

    vid-features

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import MeetingTypeList from "@/components/MeetingTypeList";
 import React from "react";
 
 const Home = () => {
@@ -11,11 +12,10 @@ const Home = () => {
     dateStyle: "full",
   }).format(now);
   return (
-    <section className="flex size-full gap-10 text-white">
+    <section className="size-full gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism min-w-[270px] rounded py-2 text-center text-base front-normal">
-            {" "}
             Upcoming Meeting at {time}
           </h2>
           <div className="flex flex-col gap-2">
@@ -24,6 +24,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <MeetingTypeList />
     </section>
   );
 };
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function MeetingTypeList() {
-  return <div>MeetingTypeList</div>;
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <div className="bg-orange-1"></div>
+      MeetingTypeList
+    </section>
+  );
 }
