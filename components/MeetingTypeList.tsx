8e9da74a6c7f58cf2d2f91636678d5ec77881c8f
stephenChinag <stephenchinag@gmail.com>
2024-08-19 13:04:47 +0100

    strem-CALL

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 import Widget from "./Widget";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "./ui/use-toast";
 
 export default function MeetingTypeList() {
   const router = useRouter();
@@ -11,7 +14,43 @@ export default function MeetingTypeList() {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting"
   >();
 
-  const createMeeting = () => {};
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [callDetails, setCallDetails] = useState<Call>();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+
+  const { toast } = useToast();
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to Create Call");
+
+      const startAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "instant Meeting";
+      await call.getOrCreate({
+        data: {
+          starts_at: startAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetails(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <section className="grid grid-cols-1 mt-10 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <Widget
--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,14 +1,43 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import { tokenProvider } from "@/server/stream.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   StreamCall,
   StreamVideo,
   StreamVideoClient,
   User,
 } from "@stream-io/video-react-sdk";
+import { ReactNode, useEffect, useState } from "react";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-const StreamVideoProvider = ({ children }) => {
-  return <StreamVideo client={client}></StreamVideo>;
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [videoCilent, setVideSetVideoClient] = useState<StreamVideoClient>();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (!isLoaded || !user) return;
+
+    if (!apiKey) throw new Error(" Stream API key missing");
+
+    const client = new StreamVideoClient({
+      apiKey,
+      user: {
+        id: user?.id,
+        name: user?.username || user?.id,
+        image: user?.imageUrl,
+      },
+      tokenProvider,
+    });
+
+    setVideSetVideoClient(client);
+  }, [user, isLoaded]);
+
+  if (!videoCilent) return <Loader />;
+
+  return <StreamVideo client={videoCilent}>{children}</StreamVideo>;
 };
 
-export default MyApp;
+export default StreamVideoProvider;
